2a4dafcd40853aa8885201e19623b8c3024d70dc
vryntrk <184443694+vryntrk@users.noreply.github.com>
2025-11-02 18:06:30 +0300

    eater update 2
    size up mechanism added and some changes applied

--- eater/eater.py
+++ eater/eater.py
@@ -17,25 +17,30 @@ background = pygame.transform.scale(pygame.image.load("sea.png"), (width, height
 hunter_width = 100
 hunter_height = 50
 hunter_vel = 4
-hunter = pygame.transform.scale(pygame.image.load("shark.png"), (hunter_width, hunter_height))
+hunter_img = pygame.image.load("shark.png")
+hunter = pygame.transform.scale(hunter_img, (hunter_width, hunter_height))
 hunter_coordinate = hunter.get_rect()
-hunter_coordinate.x = (width - hunter_width)//2
-hunter_coordinate.y = (height - hunter_height)//2
+hunter_coordinate.x = (width - hunter_width) // 2
+hunter_coordinate.y = (height - hunter_height) // 2
 
 food_width = 60
 food_height = 30
-food = pygame.transform.scale(pygame.image.load("salmon.png"), (food_width, food_height))
+food_img = pygame.image.load("salmon.png")
+food = pygame.transform.scale(food_img, (food_width, food_height))
 food_coordinate = food.get_rect()
 
 bomb_width = 50
 bomb_height = 50
-bomb = pygame.transform.scale(pygame.image.load("bomb.png"), (bomb_width, bomb_height))
+bomb_img = pygame.image.load("bomb.png")
+bomb = pygame.transform.scale(bomb_img, (bomb_width, bomb_height))
 bomb_coordinate = bomb.get_rect()
 
 font = pygame.font.SysFont("consolas", 32)
 
 eat_sound = pygame.mixer.Sound("eat.mp3")
 damage_sound = pygame.mixer.Sound("explosion.mp3")
+upgrade_sound = pygame.mixer.Sound("upgrade.mp3")
+
 pygame.mixer.music.load("background.mp3")
 pygame.mixer.music.play(-1, 0.0)
 
@@ -53,10 +58,10 @@ def draw(foods, bombs, score_point, hearts):
         win.blit(bomb, b[0])
 
     score_text = font.render(f"Score: {score_point}", 1, "White")
-    win.blit(score_text, (50, (95 - score_text.get_height())/2))
+    win.blit(score_text, (50, (95 - score_text.get_height()) // 2))
 
     remaining_hearts = font.render(f"Hearts: {hearts}", 1, "White")
-    win.blit(remaining_hearts, (width - remaining_hearts.get_width() - 50, (95 - score_text.get_height())/2))
+    win.blit(remaining_hearts, (width - remaining_hearts.get_width() - 50, (95 - score_text.get_height()) // 2))
 
     pygame.display.update()
 
@@ -72,6 +77,9 @@ def the_end(score_point):
 
 
 def main():
+    global hunter, hunter_coordinate, hunter_width, hunter_height
+    growth_point = 750
+
     run = True
     clock = pygame.time.Clock()
 
@@ -118,6 +126,20 @@ def main():
             else:
                 if elapsed_time - f[1] > 5:
                     foods.remove(f)
+
+        if score_point == growth_point:
+            upgrade_sound.play()
+
+            previous_center = hunter_coordinate.center
+
+            hunter_width += 20
+            hunter_height += 10
+
+            hunter = pygame.transform.scale(hunter_img, (hunter_width, hunter_height))
+            hunter_coordinate = hunter.get_rect()
+            hunter_coordinate.center = previous_center
+
+            growth_point += 750
 
         if bomb_count > bomb_add_increment:
             for _ in range(2):
